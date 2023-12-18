cd53c5be28571a304de675ef84e286dd80baf558
Furlos <levprimak1007@gmail.com>
2023-12-18 20:55:35 +0200

    Did for the rules of pep8

--- telegram_bot/backend/db/create_user.py
+++ telegram_bot/backend/db/create_user.py
@@ -1,15 +1,17 @@
 from pymongo import MongoClient
-client = MongoClient("localhost", 27017)
-db = client.user
+
+client = MongoClient("localhost",
+                     27017)
+db = client.users
 collection = db.users
-test1_nickname = 'eytes'
-test1_telegram_id = 12312
-def create_user(nickname:str or None, telegram_id: int):
-    if isinstance(nickname,str) == True and isinstance(nickname,None) == True and isinstance(telegram_id,int)
+
+
+def create_user(nickname: str | None, telegram_id: int):
+    if isinstance(nickname, str) == True or nickname == None \
+            and isinstance(telegram_id, int) == True:
         create_user_model = {"_id": telegram_id,
                              "name": nickname}
         db.users.insertOne(create_user_model)
         return create_user_model
     else:
-        return "Введен не правильный тип данных"
-create_user(test1_nickname,test1_telegram_id)
+        return print("Введен не правильный тип данных")
