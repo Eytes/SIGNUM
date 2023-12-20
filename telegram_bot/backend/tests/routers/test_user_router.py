60cc630351f1437c9b612480d316b549de7b2d50
Eytes <yura.shamanov2000@bk.ru>
2023-12-20 21:30:08 +0200

    вынесен отдельно тестовый клиент fastapi

--- telegram_bot/backend/tests/routers/test_user_router.py
+++ telegram_bot/backend/tests/routers/test_user_router.py
@@ -1,18 +1,13 @@
-from fastapi.testclient import TestClient
-
-from main import app
-
-
-client = TestClient(app)
+from tests.routers.fastapi_test_client import client
 
 
 def test_full_statistic():
-    response = client.get('/user/full_statistic/123123123')
+    response = client.get('/user/full_statistic/123123123', timeout=5)
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
 
 def test_min_statistic():
-    response = client.get('/user/min_statistic/123123123')
+    response = client.get('/user/min_statistic/123123123', timeout=5)
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
